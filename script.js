f527684cb3ee8128a47849dd8461becb4953d350
minviellesebastien-ctrl <minvielle.sebastien@gmail.com>
2026-08-26 13:10:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const introVideo = document.getElementById("intro-video");
+
+    const showMenu = () => {
+        document.body.classList.add("menu-visible");
+    };
+
+    if (introVideo) {
+        introVideo.addEventListener("ended", showMenu);
+        // Sécurité : Ouvre le menu au bout de 5,5 sec si la vidéo ne se lance pas
+        setTimeout(showMenu, 5500);
+    } else {
+        showMenu();
+    }
+
+    /*
+     * Redirections PWA
+     */
+    const pwaRoutes = {
+        vinyl: "https://minviellesebastien-ctrl.github.io/Vinyl_Collection/",
+        evenements: "https://minviellesebastien-ctrl.github.io/Agenda/",
+        voyages: "https://minviellesebastien-ctrl.github.io/Mes_Voyages/",
+        autre: "https://minviellesebastien-ctrl.github.io/Avenir/"
+    };
+
+    Object.keys(pwaRoutes).forEach((id) => {
+        const card = document.getElementById(id);
+        if (card) {
+            card.addEventListener("click", () => {
+                window.open(pwaRoutes[id], "_blank");
+            });
+        }
+    });
+
+});
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js");
+    });
+          }
+                          
